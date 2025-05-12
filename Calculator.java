3f3816acf8282f979f1eadeaac94df204c45ab50
wumoyu12 <147264549+wumoyu12@users.noreply.github.com>
2025-05-12 13:55:41 -0400

    Create Calculator.java

---
+++ Calculator.java
@@ -0,0 +1,23 @@
+//Title: Activity - Calculator
+//Author: Moyu Wu
+//Start Date: May 12, 2025
+//End Date: May 13, 2025
+//Description: This program will ask users to choose the operators and input two numbers they want to calculate and the program will display the answer.
+
+package Activity;
+
+public class Calculator
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Welcome to the JAVA Calculator");
+		System.out.println("------------------------------");
+		System.out.println("1 - Addition");
+		System.out.println("2 - Subtraction");
+		System.out.println("3 - Multipcation");
+		System.out.println("4 - Divsion");
+		
+		
+	}
+
+}
